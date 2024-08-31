2d16ba2ee739279e8cb2a1cb03f29d9b4c87e62e
Bhavik Sharma <130035063+bhaviksharma2004@users.noreply.github.com>
2024-08-31 18:45:25 +0530

    Add files via upload

---
+++ cell_menu/cell.h
@@ -0,0 +1,19 @@
+#ifndef CELL_H
+#define CELL_H
+
+typedef struct cell {
+    char *dataType;
+    char *colName;
+    void *data;
+    struct cell *right;
+    struct cell *down;
+    struct cell *top;
+} cell;
+
+void cellMenu(cell *topLeft, cell *bottomLeft, char *primaryKey);
+void showTable(cell *topLeft);
+void addRow(cell **bottomLeft, cell **topLeft, char **primaryKey);
+void removeRow(cell **bottomLeft, char **primaryKey);
+void removeRowByValue(cell **bottomLeft, char **primaryKey);
+
+#endif
